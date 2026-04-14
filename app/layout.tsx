fee80f5b7a8d5ee5107836a8fc3bda590e6eabcc
MJ Lee <loveautumnsky1007@gmail.com>
2026-04-14 19:46:41 +0900

    방문자수조회추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -44,6 +45,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+            <Analytics />
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
